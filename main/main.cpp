bfaefbd23025ce077711c9b2ea3c68a86ba6eb13
Engineer Sam <sammyyawsnana1@gmail.com>
2026-03-02 20:35:53 +0000

    tested the gpio of the esp32

---
+++ main/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <freertos/FreeRTOS.h>
+#include  <freertos/task.h>
+#include <driver/gpio.h>
+
+extern "C" void app_main();
+
+extern "C" void app_main(void)
+{
+const gpio_num_t ledPin= GPIO_NUM_25;
+gpio_config_t io_config{};
+io_config.mode=GPIO_MODE_OUTPUT;
+io_config.pin_bit_mask= 1ULL<<ledPin;
+io_config.pull_down_en= GPIO_PULLDOWN_DISABLE;
+io_config.pull_up_en= GPIO_PULLUP_DISABLE;
+io_config.intr_type=GPIO_INTR_DISABLE;
+gpio_config(&io_config);
+
+    while(true){
+   gpio_set_level(ledPin,1);
+   vTaskDelay(pdMS_TO_TICKS(500));
+   gpio_set_level(ledPin,0);
+   vTaskDelay(pdMS_TO_TICKS(400));
+    };
+}
